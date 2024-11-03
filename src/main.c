090a8c6b232be4f33012fb7e52d08fe8357bca1f
eliseydudin <dudinelisey630@gmail.com>
2024-11-03 21:12:43 +0300

    generate object files

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
+#include <llvm-c/BitWriter.h>
 #include <llvm-c/Core.h>
+#include <llvm-c/ExecutionEngine.h>
+#include <llvm-c/Target.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,12 +9,15 @@
 const char *program = "><<>>";
 
 int main(int argc, const char *argv[]) {
+  LLVMInitializeNativeTarget();
+  LLVMInitializeNativeAsmPrinter();
+  LLVMInitializeNativeAsmParser();
+
   LLVMContextRef context = LLVMContextCreate();
   LLVMModuleRef module = LLVMModuleCreateWithNameInContext("fkys", context);
   LLVMBuilderRef builder = LLVMCreateBuilderInContext(context);
 
   LLVMTypeRef int32_type = LLVMInt32TypeInContext(context);
-
   LLVMTypeRef main_function_type = LLVMFunctionType(int32_type, NULL, 0, 0);
   LLVMValueRef main_function =
       LLVMAddFunction(module, "main", main_function_type);
@@ -26,7 +32,6 @@ int main(int argc, const char *argv[]) {
 
   for (int i = 0; i < strlen(program); i++) {
     const char ch = program[i];
-
     LLVMValueRef current_index_value =
         LLVMBuildLoad2(builder, int32_type, index, "current_index");
 
@@ -40,7 +45,7 @@ int main(int argc, const char *argv[]) {
             LLVMBuildAdd(builder, current_index_value, llvm_one, "_index");
         break;
       default:
-        printf("Unknown character: %d\n", ch);
+        printf("Unknown character: %dn", ch);
         return 1;
     }
 
@@ -49,11 +54,50 @@ int main(int argc, const char *argv[]) {
 
   LLVMBuildRet(builder, LLVMConstInt(int32_type, 0, 0));
 
-  LLVMDumpModule(module);
+  char *error = NULL;
+  LLVMTargetRef target;
+  LLVMTargetMachineRef target_machine;
+
+  if (LLVMGetTargetFromTriple(LLVMGetDefaultTargetTriple(), &target, &error)
+      != 0) {
+    fprintf(stderr, "Failed to get target: %sn", error);
+    return 1;
+  }
+
+  target_machine = LLVMCreateTargetMachine(
+      target,
+      LLVMGetDefaultTargetTriple(),
+      "generic",
+      "",
+      LLVMCodeGenLevelDefault,
+      LLVMRelocDefault,
+      LLVMCodeModelDefault
+  );
+
+  const char *output_filename = "output.o";
+
+  if (LLVMWriteBitcodeToMemoryBuffer(module) == NULL) {
+    fprintf(stderr, "Failed to write bitcode to memory buffern\n");
+    return 1;
+  }
+
+  if (LLVMTargetMachineEmitToFile(
+          target_machine,
+          module,
+          output_filename,
+          LLVMObjectFile,
+          &error
+      )
+      != 0) {
+    fprintf(stderr, "Failed to emit object file: %s\n", error);
+    return 1;
+  }
 
   LLVMDisposeBuilder(builder);
   LLVMDisposeModule(module);
   LLVMContextDispose(context);
+
+  LLVMDisposeTargetMachine(target_machine);
 
   return 0;
 }
