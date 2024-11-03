ee97e10ee3a1984f0641d5c703c9f76f94df2559
eliseydudin <dudinelisey630@gmail.com>
2024-11-03 21:06:28 +0300

    incrementing indexes

--- src/main.c
+++ src/main.c
@@ -1,31 +1,16 @@
 #include <llvm-c/Core.h>
-#include <llvm-c/ExecutionEngine.h>
-#include <llvm-c/Target.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
-const char *program = ">---<+++";
+const char *program = "><<>>";
 
 int main(int argc, const char *argv[]) {
   LLVMContextRef context = LLVMContextCreate();
-  LLVMModuleRef module = LLVMModuleCreateWithNameInContext("fkys-jit", context);
+  LLVMModuleRef module = LLVMModuleCreateWithNameInContext("fkys", context);
   LLVMBuilderRef builder = LLVMCreateBuilderInContext(context);
 
   LLVMTypeRef int32_type = LLVMInt32TypeInContext(context);
-  LLVMTypeRef array_type = LLVMArrayType(int32_type, 500);
-
-  LLVMValueRef zero_constants[500];
-  for (int i = 0; i < 500; i++) {
-    zero_constants[i] = LLVMConstInt(int32_type, 0, 0);
-  }
-
-  LLVMValueRef global_array = LLVMAddGlobal(module, array_type, "array");
-  LLVMSetInitializer(
-      global_array,
-      LLVMConstArray(int32_type, zero_constants, 500)
-  );
-  LLVMSetGlobalConstant(global_array, 0);
 
   LLVMTypeRef main_function_type = LLVMFunctionType(int32_type, NULL, 0, 0);
   LLVMValueRef main_function =
@@ -34,25 +19,32 @@ int main(int argc, const char *argv[]) {
       LLVMAppendBasicBlockInContext(context, main_function, "entry");
   LLVMPositionBuilderAtEnd(builder, entry);
 
-  LLVMValueRef index_variable = LLVMBuildAlloca(builder, int32_type, "index");
-  LLVMBuildStore(builder, LLVMConstInt(int32_type, 0, 0), index_variable);
+  LLVMValueRef index = LLVMBuildAlloca(builder, int32_type, "index");
+  LLVMBuildStore(builder, LLVMConstInt(int32_type, 0, 0), index);
+
+  LLVMValueRef llvm_one = LLVMConstInt(int32_type, 1, 0);
 
   for (int i = 0; i < strlen(program); i++) {
     const char ch = program[i];
 
+    LLVMValueRef current_index_value =
+        LLVMBuildLoad2(builder, int32_type, index, "current_index");
+
     switch (ch) {
-      case '+':
-        break;
-      case '-':
-        break;
       case '<':
+        current_index_value =
+            LLVMBuildSub(builder, current_index_value, llvm_one, "_index");
         break;
       case '>':
+        current_index_value =
+            LLVMBuildAdd(builder, current_index_value, llvm_one, "_index");
         break;
       default:
-        printf("huh, unknown character: %d\n", ch);
+        printf("Unknown character: %d\n", ch);
         return 1;
     }
+
+    LLVMBuildStore(builder, current_index_value, index);
   }
 
   LLVMBuildRet(builder, LLVMConstInt(int32_type, 0, 0));
