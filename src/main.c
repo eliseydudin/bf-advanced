049147690618f9bb1a1ec5d378c6b708f32b70e5
eliseydudin <dudinelisey630@gmail.com>
2024-11-05 21:48:12 +0300

    full reset (this thing suuuucks)

--- src/main.c
+++ src/main.c
@@ -1,24 +1,5 @@
 #include <fkys/fkys.h>
 
 int main(int argc, const char *argv[]) {
-  struct fkys_state state;
-
-  fkys_state_new(&state);
-
-  LLVMValueRef tmp = fkys_array_get(&state);
-  for (int i = 0; i < 10; i++) {
-    fkys_incr(&state, &tmp);
-  }
-  fkys_set_index(&state, tmp);
-
-  tmp = fkys_array_get(&state);
-  fkys_return_value(&state, tmp);
-  //fkys_putchar(&state, &tmp);
-
-  fkys_return_zero(&state);
-
-  fkys_dump_to_cli(&state);
-  fkys_state_finish(&state);
-
   return 0;
 }
