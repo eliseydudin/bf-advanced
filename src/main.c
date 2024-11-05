81142d7d4035c4aecc38acac2efc7f2b35cc4496
eliseydudin <dudinelisey630@gmail.com>
2024-11-05 10:28:23 +0300

    better now

--- src/main.c
+++ src/main.c
@@ -3,7 +3,10 @@
 #include "state.h"
 
 int main(int argc, const char *argv[]) {
-  struct fkys_state state = fkys_state_new();
+  struct fkys_state state;
+
+  fkys_state_new(&state);
+  fkys_dump_to_cli(&state);
   fkys_state_finish(&state);
 
   return 0;
--- src/state.h
+++ src/state.h
@@ -2,20 +2,60 @@
 #define __STATE_H__
 
 #include <llvm-c/Core.h>
+#include <stddef.h>
+
+struct fkys_values {
+  LLVMValueRef array, index;
+};
+
+struct fkys_constants {
+  LLVMValueRef zero, one;
+};
+
+struct fkys_types {
+  LLVMTypeRef i32;
+  LLVMTypeRef main;
+};
+
+struct fkys_main {
+  LLVMBasicBlockRef entry;
+  LLVMValueRef main_fn;
+};
 
 struct fkys_state {
   LLVMContextRef ctx;
   LLVMModuleRef module;
   LLVMBuilderRef builder;
+
+  struct fkys_values values;
+  struct fkys_constants constants;
+  struct fkys_types types;
+  struct fkys_main main;
 };
 
-struct fkys_state fkys_state_new() {
-  LLVMContextRef ctx = LLVMContextCreate();
-  LLVMModuleRef module = LLVMModuleCreateWithNameInContext("fkys", ctx);
-  LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
+void fkys_state_new(struct fkys_state *state) {
+  state->ctx = LLVMContextCreate();
+  state->module = LLVMModuleCreateWithNameInContext("fkys", state->ctx);
+  state->builder = LLVMCreateBuilderInContext(state->ctx);
 
-  struct fkys_state new = {.ctx = ctx, .module = module, .builder = builder};
-  return new;
+  state->types.i32 = LLVMInt32TypeInContext(state->ctx);
+  state->types.main = LLVMFunctionType(state->types.i32, NULL, 0, 0);
+
+  state->constants.zero = LLVMConstInt(state->types.i32, 0, 0);
+  state->constants.one = LLVMConstInt(state->types.i32, 1, 0);
+
+  state->main.main_fn =
+      LLVMAddFunction(state->module, "main", state->types.main);
+  state->main.entry =
+      LLVMAppendBasicBlockInContext(state->ctx, state->main.main_fn, "entry");
+  LLVMPositionBuilderAtEnd(state->builder, state->main.entry);
+
+  state->values.index =
+      LLVMBuildAlloca(state->builder, state->types.i32, "index");
+}
+
+void fkys_dump_to_cli(struct fkys_state *state) {
+  LLVMDumpModule(state->module);
 }
 
 void fkys_state_finish(struct fkys_state *state) {
